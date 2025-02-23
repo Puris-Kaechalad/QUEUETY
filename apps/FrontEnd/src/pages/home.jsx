b1636449db4ef62f6667055ea4901ff22db20125
Puris <puris.naja.2004@gmail.com>
2025-02-23 17:31:33 +0700

    home page

--- apps/FrontEnd/src/pages/home.jsx
+++ apps/FrontEnd/src/pages/home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../component/nav'
+import Cocktail from '../assets/cocktail.jpg'
 
 function home() {
   return (
     <>
-        <Navbar/>
-        <div>home</div>
+      <Navbar/>
+      <div className="w-full h-150 relative">
+        <img src={Cocktail} alt="Background" className="w-full h-full object-cover" />
+      </div>
     </>
   )
 }
