edc7e865e715f73864d3a9b9feb27254f0e7158e
Puris <puris.naja.2004@gmail.com>
2025-03-04 21:46:49 +0700

    Login page

--- apps/FrontEnd/src/App.jsx
+++ apps/FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Register from './pages/client/register'
+import Login from './pages/client/login'
 import Home from './pages/client/home'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
--- apps/FrontEnd/src/pages/client/login.jsx
+++ apps/FrontEnd/src/pages/client/login.jsx
@@ -3,7 +3,39 @@ import Navbar from '../../component/nav'
 
 function login() {
     return (
-        <Navbar />
+        <>
+            <Navbar />
+            <div className="flex justify-center items-center min-h-screen p-8 sm:p-0">
+                <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
+                    <h2 className="text-3xl font-bold text-center text-black mb-6">Login</h2>
+                    <div className="mt-4">
+                        <label className="block text-sm font-medium text-gray-600">Email</label>
+                        <input
+                            type="email"
+                            placeholder="Enter your email..."
+                            className="input text-black w-full mt-1 p-2 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label className="block text-sm font-medium text-gray-600">Password</label>
+                        <input
+                            type="password"
+                            placeholder="Enter your password..."
+                            className="input text-black w-full mt-1 p-2 border rounded-lg bg-gray-100 focus:ring-2 focus:ring-blue-500"
+                        />
+                    </div>
+
+                    <button className="w-full mt-8 bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600">
+                        LOGIN
+                    </button>
+                    <div className="flex justify-start gap-2 mt-4">
+                        <p className="text-gray-500 text-sm">Do not have an account yet?</p>
+                        <a className="text-sky-600 text-sm">Register</a>
+                    </div>
+
+                </div>
+            </div>
+        </>
     )
 }
 
