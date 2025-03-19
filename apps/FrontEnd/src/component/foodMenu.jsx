e1f8da7c758517ee4b34c2d304fe097f714e766b
Puris <puris.naja.2004@gmail.com>
2025-03-19 19:10:54 +0700

    food menu

--- apps/FrontEnd/src/component/foodMenu.jsx
+++ apps/FrontEnd/src/component/foodMenu.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { useState } from 'react';
 import { Link } from "react-router-dom";
+import '../pages/client/client.css'
 import Menu from "../assets/menu-icon.png"
 import All from "../assets/all.png"
 import Meat from "../assets/meat.png"
@@ -10,6 +12,12 @@ import Dessert from "../assets/dessert.png"
 import Drink from "../assets/drink.png"
 
 function foodMenu() {
+    const [activePage, setActivePage] = useState(null); // สถานะ active page
+
+    const handleClick = (page) => {
+        setActivePage(page); // เมื่อคลิกจะตั้งค่าหน้า active
+    };
+
     return (
         <>
             <aside className="sidebar w-1/4">
@@ -18,44 +26,44 @@ function foodMenu() {
                         <img src={Menu} alt="icon" className="h-12" />
                         <h2 className="text-4xl text-yellow-500 font-bold">Menu</h2>
                     </div>
-                    <div className="text-lg w-full space-y-3 self-start">
-                        <button id="all" className="flex justify-between w-full" onClick={() => handleCategoryClick('All')}>
+                    <div className="text-lg w-full self-start">
+                        <Link to="/menu" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105">
                             <img src={All} alt="icon" className="h-6" />
                             <p>All</p>
-                        </button>
-                        <hr></hr>
+                        </Link>
+                        <hr className="my-1"></hr>
 
-                        <Link to="/menu/Meat" id="meat" className="flex justify-between w-full" onClick={() => handleCategoryClick('Meat')}>
+                        <Link to="/menu/Meat" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105">
                             <img src={Meat} alt="icon" className="h-6" />
                             <p>Meat</p>
                         </Link>
-                        <hr></hr>
+                        <hr className="my-1"></hr>
 
-                        <Link to="/menu/fries" id="fries" className="flex justify-between w-full" onClick={() => handleCategoryClick('Fries')}>
+                        <Link to="/menu/fries" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105">
                             <img src={Fried} alt="icon" className="h-6" />
                             <p>Fries</p>
                         </Link>
-                        <hr></hr>
+                        <hr className="my-1"></hr>
 
-                        <Link to="/menu/seaFood" id="sea" className="flex justify-between w-full" onClick={() => handleCategoryClick('Sea')}>
+                        <Link to="/menu/seaFood" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105">
                             <img src={Shrimp} alt="icon" className="h-6" />
                             <p>Sea food</p>
                         </Link>
-                        <hr></hr>
+                        <hr className="my-1"></hr>
 
-                        <Link to="/menu/fruit" id="fruit" className="flex justify-between w-full" onClick={() => handleCategoryClick('Fruit')}>
+                        <Link to="/menu/fruit" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105">
                             <img src={Apple} alt="icon" className="h-6" />
                             <p>Fruit</p>
                         </Link>
-                        <hr></hr>
+                        <hr className="my-1"></hr>
 
-                        <Link to="/menu/dessert" id="dessert" className="flex justify-between w-full" onClick={() => handleCategoryClick('Dessert')}>
+                        <Link to="/menu/dessert" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105" onClick={() => handleCategoryClick('Dessert')}>
                             <img src={Dessert} alt="icon" className="h-6" />
                             <p>Dessert</p>
                         </Link>
-                        <hr></hr>
+                        <hr className="my-1"></hr>
 
-                        <Link to="/menu/drink" id="drink" className="flex justify-between w-full" onClick={() => handleCategoryClick('Drink')}>
+                        <Link to="/menu/drink" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105" onClick={() => handleCategoryClick('Drink')}>
                             <img src={Drink} alt="icon" className="h-6" />
                             <p>Drink</p>
                         </Link>
--- apps/FrontEnd/src/pages/client/Menu/seaFood.jsx
+++ apps/FrontEnd/src/pages/client/Menu/seaFood.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Navbar from '../../../component/nav'
+import FoodMenu from '../../../component/foodMenu'
+import '../client.css'
+import Steak from "../../../assets/steak.jpg"
+import Cupcake from "../../../assets/cupcake.jpg"
 
 function seaFood() {
     return (
         <>
+            <Navbar />
+            <div className="flex ">
+                <FoodMenu />
+                <div className="content w-full py-32">
+                    <div className="card-box py-4 flex justify-center flex-wrap gap-8 tracking-widest text-lg font-semibold">
 
+                        <div className="w-64 card">
+                            <div className="p-1">
+                                <img
+                                    src={Steak}
+                                    alt="img" className="rounded-md" />
+                            </div>
+                            <div className="p-2 w-full text-center">
+                                <h2>Sea food</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
