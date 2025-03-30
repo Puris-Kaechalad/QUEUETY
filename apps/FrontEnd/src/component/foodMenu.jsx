5f947337094b6fb27e388c55f7b877795e2273bf
Puris <puris.naja.2004@gmail.com>
2025-03-30 22:00:32 +0700

    menu admin

--- apps/FrontEnd/src/component/foodMenu.jsx
+++ apps/FrontEnd/src/component/foodMenu.jsx
@@ -10,6 +10,8 @@ import Shrimp from "../assets/shrimp.png"
 import Apple from "../assets/apple.png"
 import Dessert from "../assets/dessert.png"
 import Drink from "../assets/drink.png"
+import Edit from "../assets/pencil.png";
+import Del from "../assets/bin.png";
 
 function foodMenu() {
     const [activePage, setActivePage] = useState(null); // สถานะ active page
@@ -26,7 +28,160 @@ function foodMenu() {
                         <img src={Menu} alt="icon" className="h-12" />
                         <h2 className="text-4xl text-yellow-500 font-bold">Menu</h2>
                     </div>
-                    <div className="text-lg w-full self-start">
+
+                    {/* edit, delete menu */}
+                    <div className="flex justify-between">
+                        <button className="bg-white text-sky-500 font-semibold px-4 py-1 rounded-lg flex items-center gap-2 cursor-pointer" onClick={() => document.getElementById('addMenu').showModal()}>
+                            Add
+                            <img src={Edit} alt="edit icon" className="h-4" />
+                        </button>
+
+                        <button className="bg-white text-red-500 font-semibold px-4 py-1 rounded-lg flex items-center gap-2 cursor-pointer" onClick={() => document.getElementById('deleteMenu').showModal()}>
+                            Delete
+                            <img src={Del} alt="edit icon" className="h-4" />
+                        </button>
+                    </div>
+
+                    <dialog id="addMenu" className="modal">
+                        <div className="modal-box bg-white text-black">
+                            <form method="dialog">
+                                <button className="btn bg-transparent border-none shadow-none absolute right-2 top-2 text-black">✕</button>
+                            </form>
+                            <h3 className="font-bold text-2xl">Add menu</h3>
+                            <div className="mt-8 text-center space-y-6">
+                                <div>
+                                    <details className="dropdown">
+                                        <summary className="btn bg-transparent shadow-none text-black">Category</summary>
+                                        <ul className="menu dropdown-content bg-white shadow-black shadow-lg rounded-box z-1 w-52 p-2 mt-1 shadow-sm">
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Meat</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Fries</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Sea food</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Fruit</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Dessert</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Drink</span>
+                                                </label>
+                                            </li>
+
+                                        </ul>
+                                    </details>
+                                </div>
+
+                                <label className="text-lg">Name</label>
+                                <div className="flex justify-center mt-2">
+                                    <input id="dateInput" type="text" placeholder="stake" className="w-1/4" />
+                                </div>
+
+                                <label className="text-lg">Picture</label>
+                                <div className="flex justify-center mt-2">
+                                    <input type="file" accept="image" className="w-1/3" />
+                                </div>
+                            </div>
+                            <div className="flex justify-center mt-8">
+                                <button className="bg-sky-500 px-4 py-1 text-white rounded-full hover:scale-110 duration-200 cursor-pointer">
+                                    Confirm
+                                </button>
+                            </div>
+                        </div>
+                    </dialog>
+
+                    <dialog id="deleteMenu" className="modal">
+                        <div className="modal-box bg-white text-black">
+                            <form method="dialog">
+                                <button className="btn bg-transparent border-none shadow-none absolute right-2 top-2 text-black">✕</button>
+                            </form>
+                            <h3 className="font-bold text-2xl">Delete menu</h3>
+                            <div className="mt-8 text-center space-y-6">
+                                <div>
+                                    <details className="dropdown">
+                                        <summary className="btn bg-transparent shadow-none text-black">Category</summary>
+                                        <ul className="menu dropdown-content bg-white shadow-black shadow-lg rounded-box z-1 w-52 p-2 mt-1 shadow-sm">
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Meat</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Fries</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Sea food</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Fruit</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Dessert</span>
+                                                </label>
+                                            </li>
+                                            <li>
+                                                <label className="flex items-center gap-2 cursor-pointer px-2 py-1">
+                                                    <input type="radio" name="radio-2" className="radio radio-sm" />
+                                                    <span>Drink</span>
+                                                </label>
+                                            </li>
+
+                                        </ul>
+                                    </details>
+                                </div>
+
+                                <label className="text-lg">Name</label>
+                                <div className="flex justify-center mt-2">
+                                    <input id="dateInput" type="text" placeholder="stake" className="w-1/4" />
+                                </div>
+                            </div>
+                            
+                            <div className="flex justify-center mt-8">
+                                <button className="bg-red-500 px-4 py-1 text-white rounded-full hover:scale-110 duration-200 cursor-pointer">
+                                    Confirm
+                                </button>
+                            </div>
+                        </div>
+                    </dialog>
+
+                    
+                    {/* -------------------- */}
+
+                    <div className="text-lg w-full self-start mt-4">
                         <Link to="/menu" className="menu-select flex justify-between w-full p-2 transition-all duration-200 hover:scale-105">
                             <img src={All} alt="icon" className="h-6" />
                             <p>All</p>
